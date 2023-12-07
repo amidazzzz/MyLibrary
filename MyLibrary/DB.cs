ef2812c992c630b79b30aadf6900e1a29b7eec6c
envy <cain.wells@mail.ru>
2023-12-07 17:29:24 +0300

    Initial Commit

---
+++ MyLibrary/DB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    internal class DB
+    {
+        public static MyLibraryEntities Connection = new MyLibraryEntities();
+    }
+}
---
+++ MyLibrary/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using MyLibrary.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyLibrary
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var page = new AddBookPage();
+            MainFrame.Navigate(page);
+        }
+
+        private void BooksButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var page = new BookListPage();
+            MainFrame.Navigate(page);  
+        }
+
+        private void GetBookCategories_OnClick(object sender, RoutedEventArgs e)
+        {
+            var page = new CategListPage();
+            MainFrame.Navigate(page);
+        }
+
+        private void AddCategory_OnClick(object sender, RoutedEventArgs e)
+        {
+            var page = new AddCategoryPage();
+            MainFrame.Navigate(page);
+        }
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
